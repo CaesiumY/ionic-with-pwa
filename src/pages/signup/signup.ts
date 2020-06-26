27050aef6e0e52de0d84bfa07ebd4fb7b1cef56c
CaesiumY <mn0658@naver.com>
2020-06-26 11:48:07 +0900

    feat: Add async in signup func

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core";
 import { IonicPage, NavController, NavParams } from "ionic-angular";
 import * as firebase from "firebase";
+import { LoaderProvider } from "../../providers/loader/loader";
 
 /**
  * Generated class for the SignupPage page.
@@ -21,44 +22,75 @@ export class SignupPage {
     password: "",
   };
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private loader: LoaderProvider
+  ) {}
 
   ionViewDidLoad() {
     console.log("ionViewDidLoad SignupPage");
   }
 
-  signup() {
-    firebase
-      .auth()
-      .createUserWithEmailAndPassword(this.account.email, this.account.password)
-      .then((result) => {
-        console.log("SignupPage -> signup -> result", result);
+  async signup() {
+    this.loader.show();
+
+    try {
+      const result = await firebase
+        .auth()
+        .createUserWithEmailAndPassword(
+          this.account.email,
+          this.account.password
+        );
+
+      if (result) {
+        console.log(result);
         var user = firebase.auth().currentUser;
 
-        user
-          .updateProfile({
-            displayName: this.account.name,
-            photoURL: "",
-          })
-          .then(() => {
-            // Update successful.
-            console.log("Update successful");
-          })
-          .catch((error) => {
-            // An error happened.
-            console.log("SignupPage -> signup -> error", error);
-          });
-      })
-      .catch((error) => {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        console.log(
-          "SignupPage -> signup -> errorCode",
-          errorCode,
-          errorMessage
-        );
-        // ...
-      });
+        await user.updateProfile({
+          displayName: this.account.name,
+          photoURL: "",
+        });
+      } else {
+        console.log("error");
+      }
+    } catch (error) {
+      console.warn("SignupPage -> signup -> error", error);
+    }
+
+    this.loader.hide();
+
+    // firebase
+    //   .auth()
+    //   .createUserWithEmailAndPassword(this.account.email, this.account.password)
+    //   .then((result) => {
+    //     console.log("SignupPage -> signup -> result", result);
+    //     var user = firebase.auth().currentUser;
+
+    //     user
+    //       .updateProfile({
+    //         displayName: this.account.name,
+    //         photoURL: "",
+    //       })
+    //       .then(() => {
+    //         // Update successful.
+    //         console.log("Update successful");
+    //       })
+    //       .catch((error) => {
+    //         // An error happened.
+    //         console.log("SignupPage -> signup -> error", error);
+    //       });
+    //   })
+    //   .catch((error) => {
+    //     // Handle Errors here.
+    //     var errorCode = error.code;
+    //     var errorMessage = error.message;
+    //     console.log(
+    //       "SignupPage -> signup -> errorCode",
+    //       errorCode,
+    //       errorMessage
+    //     );
+    //     // ...
+    //   });
   }
 }
