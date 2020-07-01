7cb2b2bee9e52cc0e99b4e7b125875aba96292b7
CaesiumY <mn0658@naver.com>
2020-07-01 11:21:19 +0900

    feat: Add writing to realtime database at firebase

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -2,6 +2,7 @@ import { Component } from "@angular/core";
 import { IonicPage, NavController, NavParams } from "ionic-angular";
 import * as firebase from "firebase";
 import { LoaderProvider } from "../../providers/loader/loader";
+import moment from "moment";
 
 /**
  * Generated class for the SignupPage page.
@@ -44,13 +45,31 @@ export class SignupPage {
         );
 
       if (result) {
-        console.log(result);
         var user = firebase.auth().currentUser;
 
         await user.updateProfile({
           displayName: this.account.name,
           photoURL: "",
         });
+
+        var postData = {
+          name: this.account.name,
+          email: this.account.email,
+          // password: this.account.password,
+          // NOTE - above password data is not encrypted! BE CAREFUL WHEN YOU INCLUDE IT!
+          date: moment().format("YYYY-MM-DD"),
+          id: result.uid,
+        };
+
+        // Get a key for a new Post.
+        // var newPostKey = firebase.database().ref().child("posts").push().key;
+
+        // Write the new post's data simultaneously in the posts list and the user's post list.
+        var updates = {};
+        updates["/users/" + result.uid] = postData;
+        console.log("SignupPage -> signup -> updates", updates);
+
+        firebase.database().ref().update(updates);
       } else {
         console.log("error");
       }
