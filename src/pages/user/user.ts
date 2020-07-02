33f3838e517fe2e558cd970e46bbcb97f15f1c7a
CaesiumY <mn0658@naver.com>
2020-07-02 17:20:30 +0900

    feat: Add userPage content

--- src/pages/user/user.ts
+++ src/pages/user/user.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Component } from "@angular/core";
+import { IonicPage, NavController, NavParams } from "ionic-angular";
+import * as firebase from "firebase";
+import { LoaderProvider } from "../../providers/loader/loader";
 
 /**
  * Generated class for the UserPage page.
@@ -10,16 +12,46 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 @IonicPage()
 @Component({
-  selector: 'page-user',
-  templateUrl: 'user.html',
+  selector: "page-user",
+  templateUrl: "user.html",
 })
 export class UserPage {
+  private users: any = [];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private loader: LoaderProvider
+  ) {
+    this.initPage();
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad UserPage');
-  }
+  async initPage() {
+    this.loader.show();
+    var userRef = firebase.database().ref("users/");
+    // .once("value")
+    // .then((snapshot) => {
+    //   console.log("UserPage -> initPage -> snapshot", snapshot.val());
+    // })
+    // .catch((error) => console.log("error", error));
 
+    userRef.on("value", (items: any) => {
+      console.log("loaded");
+      if (items.val()) {
+        items.forEach((item) => {
+          this.users.push({
+            name: item.val().name,
+            email: item.val().email,
+            date: item.val().date,
+            id: item.val().id,
+          });
+        });
+
+        console.log("users", this.users);
+        this.loader.hide();
+      } else {
+        console.log("No User Data");
+      }
+    });
+  }
 }
