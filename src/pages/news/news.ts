3249a220b7cb88fdc0988a3c90fa7d13123b57f8
CaesiumY <mn0658@naver.com>
2020-07-09 11:51:11 +0900

    feat: Add news database

--- src/pages/news/news.ts
+++ src/pages/news/news.ts
@@ -8,6 +8,7 @@ import {
 import { NewsModalPage } from "../news-modal/news-modal";
 import firebase from "firebase";
 import moment from "moment";
+import { LoaderProvider } from "../../providers/loader/loader";
 
 /**
  * Generated class for the NewsPage page.
@@ -22,11 +23,44 @@ import moment from "moment";
   templateUrl: "news.html",
 })
 export class NewsPage {
+  private news: any;
+
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
-    private modalCtrl: ModalController
-  ) {}
+    private modalCtrl: ModalController,
+    private loader: LoaderProvider
+  ) {
+    this.initPage();
+  }
+
+  async initPage() {
+    this.loader.show();
+    try {
+      var newsRef = firebase.database().ref("news/");
+
+      const items = await newsRef.once("value");
+      if (items) {
+        this.news = [];
+        items.forEach((item: any) => {
+          this.news.push({
+            title: item.val().title,
+            category: item.val().category,
+            source: item.val().source,
+            url: item.val().url,
+            date: item.val().date,
+            clickCount: item.val().clickCount,
+            key: item.val().key,
+          });
+        });
+      } else {
+        console.log("no news data");
+      }
+    } catch (error) {
+      console.warn("NewsPage -> initPage -> error", error);
+    }
+    this.loader.hide();
+  }
 
   add() {
     let newsModal = this.modalCtrl.create(NewsModalPage);
@@ -46,7 +80,7 @@ export class NewsPage {
       title: data.title,
       category: data.category,
       source: data.source,
-      webUrl: data.webUrl,
+      url: data.url,
       date: moment().format("YYYY-MM-DD:HH:mm:SS"),
       clickCount: 0,
       key: newPostKey,
@@ -54,7 +88,7 @@ export class NewsPage {
 
     // Write the new post's data simultaneously in the posts list and the user's post list.
     var updates = {};
-    updates["/news/" + data.key] = postData;
+    updates["/news/" + newPostKey] = postData;
     console.log("NewsPage -> updateNews -> updates", updates);
 
     firebase.database().ref().update(updates);
